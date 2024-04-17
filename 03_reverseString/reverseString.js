6a5e6d66ceba1ff08fc0c972a6229e0b71f871b8
Shoyo <shoyoisgenki@gmail.com>
2024-04-17 16:37:21 +0530

    Exercise reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newStr = ''
+    for (i = word.length - 1; i>=0; i--) {
+        let char = word.slice(i, i+1);
+        newStr += char;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
