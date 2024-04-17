5df1aea5816dff0f242331e30466aec9b6550d96
Shoyo <shoyoisgenki@gmail.com>
2024-04-17 16:16:06 +0530

    Exercise repeatString solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let myString = '';
+    for (i=0; i<times; i++) {
+        myString += word;
+    }
+    return myString;
 };
 
 // Do not edit below this line
