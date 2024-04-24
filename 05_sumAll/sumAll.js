ba19e634bc978070a24d4c0087a64e15c7e04ad1
Shoyo <shoyoisgenki@gmail.com>
2024-04-24 11:51:22 +0530

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    var c = 0;
+        if (a & b > 0) {
+        if (b>a) {
+            for (i=a; i<=b; i++) {
+                c = c + i;
+            }
+        }
+        else if (a>b) {
+            for (i=a; i>=b; i--) {
+                c = c + i;
+            }
+        }
+    }
+    else return "ERROR";
+    return c;
 };
 
 // Do not edit below this line
